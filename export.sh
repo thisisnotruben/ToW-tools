71de27d9ec5c014040dfca6ccbcdc2c65627eab4
Ruben Alvarez Reyes <rubenar1996@gmail.com>
2020-02-01 08:30:43 -0700

    changes to paths

--- export.sh
+++ export.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-/home/rubsz/tiled/Tides_of_War/scripts/export.py /home/rubsz/tiled/Tides_of_War/scripts/paths/paths.json $@
+ROOT_DIR=/home/rubsz/tiled/Tides_of_War/non_map/scripts/
+DATA=paths/paths.json
+EXEC=export.py
+
+$ROOT_DIR$EXEC $ROOT_DIR$DATA $@
