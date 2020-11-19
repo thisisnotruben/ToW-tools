b827177ced6566c1577fa3bacc1c186b8eba0e44
Ruben Alvarez Reyes <rubenar1996@gmail.com>
2020-11-18 18:47:31 -0700

    improved TiledManager; removed characters tilesets

--- core/tiled_manager.py
+++ core/tiled_manager.py
@@ -48,10 +48,7 @@ class Tiled:
     @staticmethod
     def _get_center_pos(pos=(), size=()):
         offset = round(float(Tiled.cell_size) / 2.0)
-        x = int(
-            round(
-                (int(pos[0]) + int(size[0]) / 2.0) / float(Tiled.cell_size)) *
-            float(Tiled.cell_size))
+        x = int(round((int(pos[0]) + int(size[0]) / 2.0) / float(Tiled.cell_size)) * float(Tiled.cell_size))
         x += offset
         y = int(pos[1]) + offset
         return (int(x), int(y))
@@ -71,35 +68,30 @@ class Tiled:
             base_name = unit_meta[unit_atts["id"]]["img"].split("-")[0]
         return "%s-%s" % (unit_atts["id"], base_name)
 
-    def _get_map_character_layer(self):
-        root = ET.parse(self.tiled["map_file"]).getroot()
+    def _get_map_character_layer(self, root):
         group_index = 0
         layer_index = 0
         for child in root:
             if child.tag == "group" and child.attrib["name"] == "zed":
                 for child in root[group_index]:
-                    if child.tag == "objectgroup" and child.attrib[
-                            "name"] == "characters":
+                    if child.tag == "objectgroup" and child.attrib["name"] == "characters":
                         break
                     layer_index += 1
                 break
             group_index += 1
         return (group_index, layer_index)
 
-    def _get_map_character_tilesets(self):
-        root = ET.parse(self.tiled["map_file"]).getroot()
+    def _get_map_character_tilesets(self, root) -> dict:
         tilesets = {}
-        for child in root:
-            if child.tag == "tileset" and "character" in child.attrib["source"]:
-                tilesets[int(
-                    child.attrib["firstgid"])] = child.attrib["source"]
+        for group in root:
+            if group.tag == "tileset" and "character" in group.attrib["source"]:
+                tilesets[int(group.attrib["firstgid"])] = group.attrib["source"]
         return tilesets
 
-    def _get_character_attributes(self) -> {}:
+    def _get_character_attributes(self, root) -> {}:
         master_dict = {}
-        tilesets = self._get_map_character_tilesets()
-        root = ET.parse(self.tiled["map_file"]).getroot()
-        group_index, layer_index = self._get_map_character_layer()
+        tilesets = self._get_map_character_tilesets(root)
+        group_index, layer_index = self._get_map_character_layer(root)
         os.chdir(self.tiled["map_dir"])
         for child in root[group_index][layer_index]:
             unit_attr = child.attrib
@@ -114,25 +106,22 @@ class Tiled:
             master_dict[unit_attr["id"]] = {}
             id_idx = 0
             for child in root:
-                if "id" in child.attrib and int(
-                        child.attrib["id"]) == ID - tile_id:
+                if "id" in child.attrib and int(child.attrib["id"]) == ID - tile_id:
                     for attribute in root[id_idx]:
                         if attribute.tag == "properties":
                             for attrib in attribute:
                                 attrib = attrib.attrib
-                                master_dict[unit_attr["id"]][
-                                    attrib["name"]] = attrib["value"]
+                                master_dict[unit_attr["id"]][attrib["name"]] = attrib["value"]
                         elif attribute.tag == "image":
                             tex_name = attribute.attrib["source"]
                             master_dict[unit_attr["id"]]["img"] = tex_name
                 id_idx += 1
         return master_dict
 
-    def _get_character_names(self, editor_names: bool) -> {}:
+    def _get_character_names(self, editor_names: bool, root) -> {}:
         names = {}
-        unit_meta = self._get_character_attributes()
-        group_index, layer_index = self._get_map_character_layer()
-        root = ET.parse(self.tiled["map_file"]).getroot()
+        unit_meta = self._get_character_attributes(root)
+        group_index, layer_index = self._get_map_character_layer(root)
         for child in root[group_index][layer_index]:
             unit_atts = child.attrib
             if editor_names:
@@ -143,78 +132,60 @@ class Tiled:
                 names[unit_atts["id"]] = unit_meta[unit_atts["id"]]["name"]
         return names
 
-    def _get_unit_paths(self):
+    def _get_unit_paths(self, root):
         unit_paths = {}
-        unit_spawn_locs = self._get_unit_spawn_locs()
-        for child in ET.parse(self.tiled["map_file"]).getroot():
+        unit_spawn_locs = self._get_unit_spawn_locs(root)
+        for child in root:
             if child.tag == "group" and child.attrib["name"] == "meta":
                 for sub_child in child:
-                    if sub_child.tag == "objectgroup" and sub_child.attrib[
-                            "name"] == "paths":
+                    if sub_child.tag == "objectgroup" and sub_child.attrib["name"] == "paths":
                         for path in sub_child:
-                            if not "name" in path.attrib or not path.attrib[
-                                    "name"].isnumeric():
-                                print(
-                                    "--> NO NAME SET IN PATH-ID: %s\n--> NAME NEEDS TO BE UNIT-ID\n--> ABORTING"
-                                    % path.attrib["id"])
+                            if not "name" in path.attrib or not path.attrib["name"].isnumeric():
+                                print("--> NO NAME SET IN PATH-ID: %s\n--> NAME NEEDS TO BE UNIT-ID\n--> ABORTING" % path.attrib["id"])
                                 exit(1)
                             elif not path.attrib["name"] in unit_spawn_locs:
-                                print(
-                                    "--> PATH NAME: %s DOESN'T MATCH WITH ANY CHARACTER-ID\n--> ABORTING"
-                                    % path.attrib["name"])
+                                print("--> PATH NAME: %s DOESN'T MATCH WITH ANY CHARACTER-ID\n--> ABORTING" % path.attrib["name"])
                                 exit(1)
                             point_origin = unit_spawn_locs[path.attrib["name"]]
-                            parse_points = ""
-                            points = []
+                            parse_points: str = ""
+                            points: list = []
                             for p in path:
                                 parse_points += p.attrib["points"]
                             parse_points = parse_points.split(" ")
                             for p in parse_points:
                                 p = p.split(",")
-                                p = (int(p[0]) + point_origin[0],
-                                     int(p[1]) + point_origin[1])
+                                p = (int(p[0]) + point_origin[0], int(p[1]) + point_origin[1])
                                 points.append(p)
                             unit_paths[path.attrib["name"]] = points
         return unit_paths
 
-    def _get_unit_spawn_locs(self):
+    def _get_unit_spawn_locs(self, root):
         spawn_locs = {}
-        group_index, layer_index = self._get_map_character_layer()
-        for child in ET.parse(
-                self.tiled["map_file"]).getroot()[group_index][layer_index]:
+        group_index, layer_index = self._get_map_character_layer(root)
+        for child in root[group_index][layer_index]:
             unit_atts = child.attrib
             if "width" in unit_atts:
-                spawn_loc = Tiled._get_center_pos(
-                    (unit_atts["x"], unit_atts["y"]),
-                    (unit_atts["width"], unit_atts["height"]))
+                spawn_loc = Tiled._get_center_pos((unit_atts["x"], unit_atts["y"]), (unit_atts["width"], unit_atts["height"]))
                 spawn_locs[unit_atts["id"]] = spawn_loc
         return spawn_locs
 
     def is_debugging(self):
-        return os.path.exists(
-            os.path.join(self.tiled["map_dir"], Tiled.temp_dir))
+        return os.path.exists(os.path.join(self.tiled["map_dir"], Tiled.temp_dir))
 
     def debug_map(self):
         os.chdir(self.tiled["map_dir"])
         debug = True
         if os.path.exists(Tiled.temp_dir):
-            Tiled._debug_map_move_files(Tiled.temp_dir,
-                                        self.tiled["tileset_dir"])
+            Tiled._debug_map_move_files(Tiled.temp_dir, self.tiled["tileset_dir"])
             shutil.rmtree(Tiled.temp_dir)
             debug = False
         else:
             os.mkdir(Tiled.temp_dir)
-            Tiled._debug_map_move_files(self.tiled["tileset_dir"],
-                                        Tiled.temp_dir)
-            Tiled._debug_map_move_files(self.tiled["debug_dir"],
-                                        self.tiled["tileset_dir"])
+            Tiled._debug_map_move_files(self.tiled["tileset_dir"], Tiled.temp_dir)
+            Tiled._debug_map_move_files(self.tiled["debug_dir"], self.tiled["tileset_dir"])
             with open(os.path.join(Tiled.temp_dir, "README.txt"), "w") as f:
-                f.write(
-                    "Don't delete folder or contents of folder manually. If you do, you've done messed up."
-                )
-        print(
-            "--> MAP DEBUG OVERLAY: %s\n--> PRESS (CTRL-T) TO REFRESH IF HASN'T SHOWN"
-            % debug)
+                f.write("Don't delete folder or contents of folder manually. If you do, you've done messed up.")
+        print("--> MAP DEBUG OVERLAY: %s\n--> PRESS (CTRL-T) TO REFRESH IF HASN'T SHOWN" % debug)
 
     def make_debug_tilesets(self):
         defined_color_names = [c.name for c in Color]
@@ -223,9 +194,7 @@ class Tiled:
             if tileset.endswith(Tiled.img_ext):
                 # check if color is defined
                 if not self.debug[tileset] in defined_color_names:
-                    print(
-                        "--> COLOR: (%s) NOT DEFINED FOR TILESET: (%s)\n--> DEFINED COLORS:"
-                        % (self.debug[tileset], tileset))
+                    print("--> COLOR: (%s) NOT DEFINED FOR TILESET: (%s)\n--> DEFINED COLORS:" % (self.debug[tileset], tileset))
                     for color in defined_color_names:
                         print(" |-> ", color)
                     print("--> ABORTING")
@@ -234,8 +203,7 @@ class Tiled:
                 src = os.path.join(self.tiled["tileset_dir"], tileset)
                 dest = os.path.join(self.tiled["debug_dir"], tileset)
                 # create overlay
-                ImageEditor.create_overlay(
-                    src, dest, Color[self.debug[tileset]].value)
+                ImageEditor.create_overlay(src, dest, Color[self.debug[tileset]].value)
                 print(" |-> TILESET MADE: (%s)" % dest)
         print("--> ALL DEBUG TILESETS MADE")
 
@@ -244,12 +212,12 @@ class Tiled:
         for tileset in os.listdir(self.tiled["tileset_dir"]):
             if tileset in self.tilesets_32:
                 # make paths
-                src = os.path.join(self.tiled["tileset_dir"], tileset)
-                new_file_name = "%s_32%s" % (os.path.splitext(tileset)[0],
-                                             Tiled.img_ext)
-                dest = os.path.join(self.tiled["tileset_dir"], new_file_name)
+                new_file_name: str = "%s_32%s" % (os.path.splitext(tileset)[0], Tiled.img_ext)
+                src: str = os.path.join(self.tiled["tileset_dir"], tileset)
+                dest: str = os.path.join(self.tiled["tileset_dir"], new_file_name)
                 # make 32 image
                 ImageEditor.pad_height(src, dest, 16, 16)
+
                 print(" |-> TILESET MADE: (%s)" % dest)
         print("--> ALL 32px TILESETS MADE")
 
@@ -267,9 +235,7 @@ class Tiled:
             # checking if input are valid files & images
             for img in batch_order:
                 if not os.path.isfile(img):
-                    print(
-                        "--> ERROR: (sprite_paths) CANNOT CONTAIN DIRECTOR(Y/IES)\n--> ABORTING"
-                    )
+                    print("--> ERROR: (sprite_paths) CANNOT CONTAIN DIRECTOR(Y/IES)\n--> ABORTING")
                     return
         print("--> MAKING SPRITE ICONS")
         # load img db
@@ -312,16 +278,16 @@ class Tiled:
 
     def _get_character_map_data(self):
         master_dict = {}
-        root = ET.parse(self.tiled["map_file"]).getroot()    
+        root = ET.parse(self.tiled["map_file"]).getroot()
         # get unit attributes
-        group_index, layer_index = self._get_map_character_layer()
+        group_index, layer_index = self._get_map_character_layer(root)
         unit_attribute_index = 0
-        unit_meta = self._get_character_attributes()
-        unit_patrol_paths = self._get_unit_paths()
-        spawn_locs = self._get_unit_spawn_locs()
+        unit_meta = self._get_character_attributes(root)
+        unit_patrol_paths = self._get_unit_paths(root)
+        spawn_locs = self._get_unit_spawn_locs(root)
         layer = root[group_index][layer_index]
-        editor_names = self._get_character_names(True)
-        game_names = self._get_character_names(False)
+        editor_names = self._get_character_names(True, root)
+        game_names = self._get_character_names(False, root)
 
         for unit_index in range(len(layer)):
             unit_atts = layer[unit_index].attrib
@@ -349,14 +315,12 @@ class Tiled:
                             for tag in Tiled.tagTypes.keys():
                                 attribute_name = unit_attribute.attrib["name"]
                                 if attribute_name == tag:
-                                    
+
                                     # don't want to replace custom name is default name
                                     if tag == "name" and master_dict[unit_ID]["name"].strip() != "":
                                         continue
-
                                     # parse tag to appropriate data type
-                                    master_dict[unit_ID][attribute_name] = \
-                                        Tiled.tagTypes[tag](unit_attribute.attrib["value"])
+                                    master_dict[unit_ID][attribute_name] = Tiled.tagTypes[tag](unit_attribute.attrib["value"])
 
                         unit_attribute_index += 1
                     unit_attribute_index = 0
@@ -379,9 +343,10 @@ class Tiled:
     def _export_map(self):
         tree = ET.parse(self.tiled["map_file"])
         root = tree.getroot()
-        group_index, layer_index = self._get_map_character_layer()
-        editor_names = self._get_character_names(True)
-        spawn_locs = self._get_unit_spawn_locs()
+        group_index, layer_index = self._get_map_character_layer(root)
+        editor_names = self._get_character_names(True, root)
+        spawn_locs = self._get_unit_spawn_locs(root)
+
         # for characters
         for unit in root[group_index][layer_index]:
             unit_id = unit.attrib["id"]
@@ -389,40 +354,38 @@ class Tiled:
             if "width" in unit.attrib:
                 unit.set("x", str(spawn_locs[unit_id][0]))
                 unit.set("y", str(spawn_locs[unit_id][1]))
+
         # for target_dummys
         tD_size = ()
         for template in os.listdir(self.tiled["template_dir"]):
             if "target_dummy" in template:
-                tD_root = ET.parse(
-                    os.path.join(self.tiled["template_dir"],
-                                 template)).getroot()
+                tD_root = ET.parse(os.path.join(self.tiled["template_dir"], template)).getroot()
                 for child in tD_root:
                     if child.tag == "object":
-                        tD_size = (int(child.attrib["width"]),
-                                   int(child.attrib["height"]))
+                        tD_size = (int(child.attrib["width"]), int(child.attrib["height"]))
         if bool(tD_size):
             for group in root:
                 if group.tag == "group" and group.attrib["name"] == "zed":
                     for layer in group:
-                        if layer.tag == "objectgroup" and layer.attrib[
-                                "name"] == "target_dummys":
+                        if layer.tag == "objectgroup" and layer.attrib["name"] == "target_dummys":
                             for td in layer:
                                 td_atts = td.attrib
-                                spawn_loc = Tiled._get_center_pos(
-                                    (td_atts["x"], td_atts["y"]), tD_size)
+                                spawn_loc = Tiled._get_center_pos((td_atts["x"], td_atts["y"]), tD_size)
                                 td.set("x", str(spawn_loc[0]))
                                 td.set("y", str(spawn_loc[1]))
                                 td.set("name", Tiled._format_name({}, td_atts))
+
         # for lights and graves
         for tag_name in ["light", "grave"]:
             for group in root:
                 if group.tag == "group" and group.attrib["name"] == "meta":
                     for layer in group:
-                        if layer.tag == "objectgroup" and tag_name in layer.attrib[
-                                "name"]:
+                        if layer.tag == "objectgroup" and tag_name in layer.attrib["name"]:
                             for thing in layer:
-                                thing.set("name",
-                                          Tiled._format_name({}, thing.attrib))
+                                thing.set("name", Tiled._format_name({}, thing.attrib))
+
+        self._removeCharacterTilsets(root)
+
         # write to map
         dest = os.path.join(self.game["map_dir"], self.file_name + Tiled.map_ext)
         Tiled._write_xml(tree, dest)
@@ -437,8 +400,7 @@ class Tiled:
 
                 unit_editor_name : str = master_dict[unit_ID]["editorName"]
                 del master_dict[unit_ID]["editorName"]
-                if not (unit_editor_name.split("-")[0] in Tiled.special_units
-                        and len(master_dict[unit_ID]) == 1):
+                if not (unit_editor_name.split("-")[0] in Tiled.special_units and len(master_dict[unit_ID]) == 1):
                     clean_dict[unit_editor_name] = master_dict[unit_ID]
 
         master_dict = clean_dict
@@ -456,11 +418,20 @@ class Tiled:
                 os.path.join(self.tiled["map_dir"], map_file)
                 for map_file in os.listdir(self.tiled["map_dir"])
                 if map_file.endswith(Tiled.map_ext)
-                ]
+            ]
         for map_file in map_paths:
             self.tiled["map_file"] = map_file
             self.file_name = os.path.splitext(os.path.basename(map_file))[0]
             # export
             self._export_map()
             self._export_meta()
+
+    def _removeCharacterTilsets(self, root) -> None:
+        tilesetsToDelete: list = []
+        for group in root:
+            if group.tag == "tileset" and "character" in group.attrib["source"]:
+                tilesetsToDelete.append(group)
+
+        for tileset in tilesetsToDelete:
+            root.remove(tileset)
 
